277054e3534496fbcc1b9d22d5951cf2254a3779
Nick <79186371+Photonizilla@users.noreply.github.com>
2025-03-19 09:43:19 +0800

    Spigot algorithm

---
+++ spigot.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#define MAX_C 350000
+int a=10000, b, c=MAX_C, d, e, f[MAX_C+1], g;
+int main(int argc, char* argv[]) {
+	printf("%.20lf\n", M_PI);
+    clock_t start = clock();
+    for(;b-c;)
+        f[b++]=a/5;
+    for(; d=0, g=c*2; c -= 14, printf("%.4d",e+d/a), e=d%a)
+        for(b=c; d+=f[b]*a, f[b]=d%--g, d/=g--, --b; d*=b);
+    clock_t stop = clock();
+    printf("\nTime: %fs\n", (stop - start) / (float)CLOCKS_PER_SEC);
+    return 0;
+}
